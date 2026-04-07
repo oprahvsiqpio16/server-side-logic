57322b7ae764bb165c13f62a7399151539d4ae98
oprahvsiqpio16 <a77028488@gmail.com>
2026-04-08 02:45:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
-module.exports = {
+const data = {
   token: "8649425491:AAFLlFngt5Id3sdQOHrNVBVrhYScq8E9jOE",
-  id:"7984067238",
-  address:"https://server-side-logic-1.onrender.com"
+  id: "7984067238",
+  address: "https://server-side-logic-1.onrender.com"
 };
+
+module.exports = data;
